d1016c83b697c09a6b580188eaf50fd21fa72f8e
Tomas <tmercadoslp@gmail.com>
2024-10-09 18:56:02 -0300

    chore: add env file to use env vars with type safety

---
+++ env.ts
@@ -0,0 +1,30 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  /*
+   * Serverside Environment variables, not available on the client.
+   * Will throw if you access these variables on the client.
+   */
+  server: {
+    SHOPIFY_STORE_DOMAIN: z.string().url(),
+    SHOPIFY_STOREFRONT_ACCESS_TOKEN: z.string().min(1),
+  },
+  /*
+   * Environment variables available on the client (and server).
+   *
+   * 💡 You'll get type errors if these are not prefixed with NEXT_PUBLIC_.
+   */
+  client: {},
+  /*
+   * Due to how Next.js bundles environment variables on Edge and Client,
+   * we need to manually destructure them to make sure all are included in bundle.
+   *
+   * 💡 You'll get type errors if not all variables from `server` & `client` are included here.
+   */
+  runtimeEnv: {
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+    SHOPIFY_STOREFRONT_ACCESS_TOKEN:
+      process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+  },
+});
--- services/shopify/index.ts
+++ services/shopify/index.ts
@@ -1,20 +1,21 @@
+import { env } from "@/env";
+
+const SHOPIFY_STORE_DOMAIN = env.SHOPIFY_STORE_DOMAIN;
+const SHOPIFY_ACCESS_TOKEN = env.SHOPIFY_STOREFRONT_ACCESS_TOKEN;
+
 export async function shopifyFetch({ query }: { query: string }) {
-  const endpoint = process.env.SHOPIFY_STORE_DOMAIN;
-  const key = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN;
-
-  if (!endpoint || !key) {
-    throw new Error("Missing Shopify credentials");
-  }
-
   try {
-    const result = await fetch(`${endpoint}/api/2024-10/graphql.json`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Shopify-Storefront-Access-Token": key,
-      },
-      body: JSON.stringify({ query, variables: null }),
-    });
+    const result = await fetch(
+      `${SHOPIFY_STORE_DOMAIN}/api/2024-10/graphql.json`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Shopify-Storefront-Access-Token": SHOPIFY_ACCESS_TOKEN,
+        },
+        body: JSON.stringify({ query, variables: null }),
+      }
+    );
 
     return {
       status: result.status,
