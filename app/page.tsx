c437e68f81e1c819c22ec87880cdc0aa15f55173
Tomas <tmercadoslp@gmail.com>
2024-10-09 20:00:17 -0300

    feat: show a full product getting more data and transforming it with an schema

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,39 @@
 import { getProducts } from "@/services/shopify/queries/getProducts";
+import Image from "next/image";
 
 export default async function Home() {
-  console.log(await getProducts());
-  return <>Holis</>;
+  const productsResult = await getProducts();
+
+  if (!productsResult.success) {
+    return <>{productsResult.error}</>;
+  }
+
+  return (
+    <div className="flex flex-col gap-6">
+      <p>Products:</p>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {productsResult.body.map((product) => {
+          return (
+            <div key={product.id} className="flex flex-col gap-2">
+              {product.images.map((image) => {
+                return (
+                  <Image
+                    key={image.url}
+                    src={image.url}
+                    alt={image.altText || "Product image"}
+                    width={200}
+                    height={200}
+                  />
+                );
+              })}
+              <p>{product.title}</p>
+              <p>{product.description}</p>
+              <p>{product.price}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- services/shopify/queries/getProducts.ts
+++ services/shopify/queries/getProducts.ts
@@ -1,17 +1,80 @@
+import { z } from "zod";
 import { shopifyFetch } from "..";
 
 export async function getProducts() {
   return shopifyFetch({
-    query: `{
-        products(sortKey: TITLE, first: 100) {
-          edges{
+    query: `
+      query getProducts($title: String) {
+        products(sortKey: TITLE, first: 100, query: $title) {
+          edges {
             node {
               id
               title
               description
+              priceRange {
+                maxVariantPrice {
+                  amount
+                }
+              }
+              media(first: 100) {
+                nodes {
+                  ... on MediaImage {
+                    image {
+                      url
+                      altText
+                    }
+                  }
+                }
+              }
             }
           }
         }
-      }`,
+      }
+    `,
+    resultSchema: z
+      .object({
+        data: z.object({
+          products: z.object({
+            edges: z.array(
+              z.object({
+                node: z.object({
+                  id: z.string().min(1),
+                  title: z.string().min(1),
+                  description: z.string(),
+                  priceRange: z.object({
+                    maxVariantPrice: z.object({
+                      amount: z.string(),
+                    }),
+                  }),
+                  media: z.object({
+                    nodes: z.array(
+                      z.object({
+                        image: z.object({
+                          url: z.string(),
+                          altText: z.string().nullable(),
+                        }),
+                      })
+                    ),
+                  }),
+                }),
+              })
+            ),
+          }),
+        }),
+      })
+      .transform((data) => {
+        return data.data.products.edges.map((edge) => {
+          return {
+            id: edge.node.id,
+            title: edge.node.title,
+            description: edge.node.description,
+            price: edge.node.priceRange.maxVariantPrice.amount,
+            images: edge.node.media.nodes.map((node) => ({
+              url: node.image.url,
+              altText: node.image.altText,
+            })),
+          };
+        });
+      }),
   });
 }
