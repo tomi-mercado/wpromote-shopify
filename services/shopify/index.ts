5a5376157872195f0e731afd446bd72519567ff2
Tomas <tmercadoslp@gmail.com>
2024-10-10 19:14:38 -0300

    refactor(services/shopify): less nested try catch to make it more understandable

--- services/shopify/index.ts
+++ services/shopify/index.ts
@@ -31,75 +31,16 @@ export async function shopifyFetch<Schema extends z.ZodSchema>({
   variables?: string;
   resultSchema: Schema;
 }): Promise<ShopifyFetchSuccess<Schema> | ShopifyFetchError> {
+  let result: Response;
   try {
-    const result = await fetch(
-      `${SHOPIFY_STORE_DOMAIN}/api/2024-10/graphql.json`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "X-Shopify-Storefront-Access-Token": SHOPIFY_ACCESS_TOKEN,
-        },
-        body: JSON.stringify({ query, variables }),
-      }
-    );
-
-    let jsonResult;
-    try {
-      jsonResult = await result.json();
-    } catch (error) {
-      console.error("Error parsing JSON:", error);
-      return {
-        success: false,
-        errorCode: "JSON_PARSE_ERROR",
-        error: "Result is not JSON",
-      } as const;
-    }
-
-    if (result.status === 200 && "data" in jsonResult) {
-      let parsedResult: z.infer<Schema>;
-      try {
-        parsedResult = resultSchema.parse(jsonResult);
-      } catch (error) {
-        console.log(jsonResult);
-        console.error("Error parsing result:", error);
-        return {
-          success: false,
-          errorCode: "ZOD_PARSE_ERROR",
-          error: "Error parsing result",
-        } as const;
-      }
-
-      return {
-        success: true,
-        body: parsedResult,
-      } as const;
-    }
-
-    const errors = z
-      .object({
-        errors: z.array(
-          z.object({
-            message: z.string(),
-          })
-        ),
-      })
-      .safeParse(jsonResult);
-
-    if (errors.success) {
-      return {
-        success: false,
-        errorCode: "GRAPHQL_ERROR",
-        error: errors.data.errors.map((error) => error.message).join(", "),
-      } as const;
-    }
-
-    console.error("Unknown error:", jsonResult);
-    return {
-      success: false,
-      errorCode: "UNKNOWN_ERROR",
-      error: "Unknown error",
-    };
+    result = await fetch(`${SHOPIFY_STORE_DOMAIN}/api/2024-10/graphql.json`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Shopify-Storefront-Access-Token": SHOPIFY_ACCESS_TOKEN,
+      },
+      body: JSON.stringify({ query, variables }),
+    });
   } catch (error) {
     console.error("Error:", error);
     return {
@@ -108,4 +49,62 @@ export async function shopifyFetch<Schema extends z.ZodSchema>({
       error: "Error receiving data",
     } as const;
   }
+
+  let jsonResult;
+  try {
+    jsonResult = await result.json();
+  } catch (error) {
+    console.error("Error parsing JSON:", error);
+    return {
+      success: false,
+      errorCode: "JSON_PARSE_ERROR",
+      error: "Result is not JSON",
+    } as const;
+  }
+
+  if (result.status === 200 && "data" in jsonResult) {
+    let parsedResult: z.infer<Schema>;
+    try {
+      parsedResult = resultSchema.parse(jsonResult);
+    } catch (error) {
+      console.log(jsonResult);
+      console.error("Error parsing result:", error);
+      return {
+        success: false,
+        errorCode: "ZOD_PARSE_ERROR",
+        error: "Error parsing result",
+      } as const;
+    }
+
+    return {
+      success: true,
+      body: parsedResult,
+    } as const;
+  }
+
+  const errors = z
+    .object({
+      errors: z.array(
+        z.object({
+          message: z.string(),
+        })
+      ),
+    })
+    .safeParse(jsonResult);
+
+  if (errors.success) {
+    console.error("GraphQL error:", errors.data.errors);
+    return {
+      success: false,
+      errorCode: "GRAPHQL_ERROR",
+      error: errors.data.errors.map((error) => error.message).join(", "),
+    } as const;
+  }
+
+  console.error("Unknown error:", jsonResult);
+  return {
+    success: false,
+    errorCode: "UNKNOWN_ERROR",
+    error: "Unknown error",
+  };
 }
