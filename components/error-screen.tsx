eab63650576c0347ba56869b5c7e801268d6495a
Tomas <tmercadoslp@gmail.com>
2024-10-11 10:12:00 -0300

    feat(components): add error-screen

---
+++ components/error-screen.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+export const ErrorScreen = ({
+  title = "Error",
+  description = "An error occurred",
+}: {
+  title?: string;
+  description?: string;
+}) => {
+  return (
+    <div className="flex flex-col items-center gap-6">
+      <div className="rounded-full p-10 w-fit h-fit bg-primary">
+        <Image
+          src="/dead-plant-illustration.png"
+          alt="Dead plant illustration"
+          width={200}
+          height={200}
+        />
+      </div>
+
+      {(title || description) && (
+        <div className="text-center text-foreground-secondary">
+          {title && <p className="text-2xl font-semibold">{title}</p>}
+          {description && <p className="text-lg">{description}</p>}
+        </div>
+      )}
+    </div>
+  );
+};
